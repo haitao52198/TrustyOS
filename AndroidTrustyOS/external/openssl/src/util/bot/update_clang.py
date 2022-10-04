fc670a66910a62928299f1c0b774921e16f02c14
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 07:13:40 +0000

    Adding numeric_owner as keyword arguement

--- AndroidTrustyOS/external/openssl/src/util/bot/update_clang.py
+++ AndroidTrustyOS/external/openssl/src/util/bot/update_clang.py
@@ -76,7 +76,7 @@ def main(args):
               if not is_within_directory(path, member_path):
                   raise Exception("Attempted Path Traversal in Tar File")
       
-          tar.extractall(path, members, numeric_owner) 
+          tar.extractall(path, members, numeric_owner=numeric_owner) 
           
       
       safe_extract(tar_file, LLVM_BUILD_DIR)
